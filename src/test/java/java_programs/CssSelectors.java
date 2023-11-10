ed7a7898d9099afc8c44503c2d6b59acad1591ec
Azhagar-G <actioncvf1996@gmail.com>
2023-11-10 11:32:00 +0530

    Newly added

--- src/test/java/java_programs/CssSelectors.java
+++ src/test/java/java_programs/CssSelectors.java
@@ -1,5 +1,6 @@
 package java_programs;
 
+//import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import org.openqa.selenium.WebDriver;
@@ -7,18 +8,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class CssSelectors {
-public void Locators() {
-
-	ArrayList[] s= {"asd,asw,asde"};
+public class CssSelectors { //class----> public/protected/private  - access modifier ,abstract & final  - Specifier, No return type, class Name
+	//abstract class not able to create object
 	
+	{ //Static Block ---initialize, whenever create object for the class
+		
+		System.out.println("Static Block");
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver=new ChromeDriver();
+//	driver.get("https://www.spicejet.com/");	
+	
+		this.Locators();  //this --->keyword for access inner class & variable
+	//super ---> Keyword for access parent class & variable
+
+	}
+public String Locators() { //----> Static method can access without creating object--by calling method name in main class
+
+	System.out.println("Css Selectors");
+String a="return";
+return a;
 }
 
 public static void main(String[] args) {
-	WebDriverManager.chromedriver().setup();
-	WebDriver driver=new ChromeDriver();
-driver.get("https://www.spicejet.com/");	
-
+	CssSelectors m=new CssSelectors();
+//	m.Locators();
 }
 
 
--- src/test/java/java_programs/javaprogram.java
+++ src/test/java/java_programs/javaprogram.java
@@ -6,6 +6,65 @@ import java.util.Set;
 
 public class javaprogram {
 	public static void main(String[] args) {
+			 		        // Create a string variable with default value   
+		        String str = "agilysisasb";//"JavaTpoint is the best learning website";   
+		        //call removeDuplicates() method for removing duplicate characters    
+		        String s=removeDuplicates(str);   
+		reverse(s);   
+	triangle(5);	
+	}   
+			
+	public static String removeDuplicates(String str)   
+    {           //Create an empty string   
+        String newstr = new String();   
+                  //calculate length of the original string  
+        int length = str.length();   
+                  // Traverse the string and check for the repeated characters   
+        for (int i = 0; i <= length-1; i++)    
+        {             // store the character available at ith index in the string  
+            char charAtPosition = str.charAt(i);   
+                    // check the index of the charAtPosition. If the indexOf() method returns true add it to the resuting string  
+            if (newstr.indexOf(charAtPosition) < 0)   
+            {   
+                newstr += charAtPosition;   
+            }        }   
+        //Print string after removing duplicate characters   
+        System.out.println(newstr);  
+return(newstr);
+    }   
+	
+public static void reverse(String sr) 
+    {   for(int i=sr.length()-1;i>=0;i--) {
+System.out.print(sr.charAt(i));
+} System.out.println();
+}     
+  
+public static void triangle(int rows) {
+	 for (int i = 1; i <= rows; i++) {
+		  
+         // loop to print the number of spaces before the star
+         for (int j = rows; j >= i; j--) {
+             System.out.print("1");
+         }
+
+         // loop to print the number of stars in each row
+         for (int j = 1; j <= i; j++) {
+             System.out.print("* ");
+         }
+
+         // for new line after printing each row
+         System.out.println();
+     }
+	 
+	 //removing two Spaces
+	 String before= "trim  and strong  gun";
+	 String after = before.trim().replaceAll(" +", " ");
+	 System.out.println(after);
+}
+
+
+	
+	public void duplicate() {
 
 System.out.println("To find the duplicate string");
 		String s = "Big black bug bit a black dog on his big black bit nose";
@@ -96,18 +155,44 @@ for (String r1 : b) {
 					System.out.println(cs[i]);
 					break;}}}
 		
-	System.out.println("--------------------remove duplicate-----------");
-		String s12 = "Big black bug bit a black dog on his big black bit nose";
-		String Split12[]=s12.split(" ");
-		Set<String> b2 = new LinkedHashSet<String>();
-		for(int i=0;i<=Split12.length-1;i++) {
-		b2.add(Split12[i]);
+		System.out.println("-----------------------------------String reverse with duplicate-------------------------");
+		
+		String nn1 = null;
+		String ss1="Agilysys";
+//		String St12[]=ss1.split(" ");
+		char[] St12=ss1.toCharArray();
+		System.out.println(St12);
+		for(int i1=0;i1<=ss1.length();i1++) {
+
+	for(int j1=1;j1<=ss1.length();j1++) {
+	
+//			for(int i=ss1.length();i>=0;i--) {
+////				System.out.println("33");
+//			for(int j=ss1.length()-1;j>0;j--) {
+//				if(!(St12[i]==St12[j])) {
+					System.out.println(St12[i1]);
+				}
 		}
-//		StringBuilder sb11=new StringBuilder();
-		for(String r2:b2) {
-//		sb11.append(" "+r2);
-			System.out.print(" "+r2);
-		}}}
+		
+	}}
+//		System.out.println(nn1);
+//		System.out.println(St12);
+		
+		
+		
+//		
+//	System.out.println("--------------------remove duplicate-----------");
+//		String s12 = "Big black bug bit a black dog on his big black bit nose";
+//		String Split12[]=s12.split(" ");
+//		Set<String> b2 = new LinkedHashSet<String>();
+//		for(int i=0;i<=Split12.length-1;i++) {
+//		b2.add(Split12[i]);
+//		}
+////		StringBuilder sb11=new StringBuilder();
+//		for(String r2:b2) {
+////		sb11.append(" "+r2);
+//			System.out.print(" "+r2);
+//		}}}
 //		System.out.println(sb11);
 		
 		
@@ -177,7 +262,4 @@ for (String r1 : b) {
 //		
 //	}
 //}
-//
-//
-//
-//
+
